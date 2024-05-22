1ee4e89c49c50de4912419b63d2a47bd014fbfa2
hyeong1 <mi75625@gmail.com>
2024-05-22 12:44:19 +0900

    feat: syscall do_mmap 구현
    Co-authored-by: Jwirang._. <Jwirang@users.noreply.github.com>
    Co-authored-by: jayjayppark <jayjayppark@users.noreply.github.com>

--- vm/file.c
+++ vm/file.c
@@ -1,6 +1,8 @@
 /* file.c: Implementation of memory backed file object (mmaped object). */
 
+#include <string.h>
 #include "vm/vm.h"
+#include "include/threads/vaddr.h"
 
 static bool file_backed_swap_in (struct page *page, void *kva);
 static bool file_backed_swap_out (struct page *page);
@@ -46,10 +48,51 @@ file_backed_destroy (struct page *page) {
 	struct file_page *file_page UNUSED = &page->file;
 }
 
+static bool
+lazy_load_segment (struct page *page, void *aux) {
+	struct load_segment_aux *con = aux;
+
+	// 파일로부터 con->read_bytes만큼 데이터를 읽어 페이지 프레임에 씁니다.
+	if (file_read_at(con->file, page->frame->kva, con->read_bytes, con->ofs) != con->read_bytes)
+		return false;
+
+	// 페이지에 남은 부분은 0으로 초기화합니다.
+	memset(page->frame->kva + con->read_bytes, 0, con->zero_bytes);
+
+	return true;
+}
+
 /* Do the mmap */
 void *
 do_mmap (void *addr, size_t length, int writable,
 		struct file *file, off_t offset) {
+	size_t read_bytes = length;
+
+	while (read_bytes > 0) {
+		/* Do calculate how to fill this page.
+		 * We will read PAGE_READ_BYTES bytes from FILE
+		 * and zero the final PAGE_ZERO_BYTES bytes. */
+		size_t page_read_bytes = read_bytes < PGSIZE ? read_bytes : PGSIZE;
+		size_t page_zero_bytes = PGSIZE - page_read_bytes;
+
+		/* TODO: Set up aux to pass information to the lazy_load_segment. */
+		struct load_segment_aux *aux = (struct load_segment_aux *)malloc(sizeof(struct load_segment_aux));
+		aux->file = file;
+		aux->ofs = offset;
+		aux->read_bytes = page_read_bytes;
+		aux->zero_bytes = page_zero_bytes;
+
+		if (!vm_alloc_page_with_initializer (VM_FILE, addr,
+					writable, lazy_load_segment, aux)) {
+			return NULL;
+		}
+
+		/* Advance. */
+		read_bytes -= page_read_bytes;
+		addr += PGSIZE;
+		offset += page_read_bytes; 
+	}
+	return addr;
 }
 
 /* Do the munmap */
